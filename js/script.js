128e4ebe3f36fc7fc73639d4fe7bfc257f01f67b
sergei <sergei.avramenko1992@yandex.ru>
2022-12-05 23:23:12 +0300

    cklock

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
     
-  
+  //Tabs
      const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector(".tabheader__items");
@@ -37,6 +37,49 @@ window.addEventListener("DOMContentLoaded", () => {
           });
         }
      });
+     //Timer
+     const deadLine = "2023-05-11";
+     function getTimeRemaining(endtime) {
+       const t = Date.parse(endtime) - Date.parse(newDate()),
+             days = math.floor(t / (1000 * 60 * 60 * 24)),
+             hours = math.floor((t / (1000 * 60 * 60 ) % 24)),
+             minutes = math.floor((t / 1000 / 60) % 60),
+             seconds = math.floor((t / 1000) % 60);
+
+      return {
+          "total" : t,
+          "days" : days,
+          "hours" : hours,
+          "minutes" : minutes,
+          "seconds" : seconds
+      };
+     }
+      function setClock (selector, endtime) {
+          const timer = document.querySelector('selector'),
+                days = timer.querySelector('#days'),
+                hours = timer.querySelector('#hours'),
+                minutes = timer.querySelector('#minutes'),
+                seconds = timer.querySelector('#seconds'),
+                timeInterval = setInterval(updateClock, 1000);
+
+          function updateClock () {
+              const t = getTimeRemaining(endtime);
+              days.innerHTML = t.days;
+              hours.innerHTML = t.hours;
+              minutes.innerHTML = t.minutes;
+              seconds.innerHTML = t.seconds;
+
+              if (t.total <= 0) {
+                clearInterval(timeInterval);
+              }
+
+
+          }
+
+     }
+     setClock('.timer', deadLine);
+
+
 
 
 
