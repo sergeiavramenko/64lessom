48cc993c2ed9dce8ec405a7ed568ab75eb9533e1
sergei <sergei.avramenko1992@yandex.ru>
2022-12-08 20:51:56 +0300

    79 lesonn

--- js/script.js
+++ js/script.js
@@ -134,7 +134,7 @@ tabsParent.addEventListener('click', function(event) {
           closeModal();
       }
   });
-const modalTimerId = setTimeout(openModal, 5000);
+//const modalTimerId = setTimeout(openModal, 5000);
 
 function showmodalByScroll () {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
@@ -145,9 +145,56 @@ function showmodalByScroll () {
 }
 window.addEventListener('scroll', showmodalByScroll);
 
+ //Испольщуем классы
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.transfer = 27;
+            this.changeToUAH();
+            this.parent = document.querySelector(parentSelector);
+        }
+        changeToUAH() {
+            this.price *=this.transfer;
+        }
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        </div>
+            `;
+
+            this.parent.append(element);
+
+        }
+ }
+
+ new MenuCard(
 
 
+
+
+    
+ ).render();
 
 
 });
 console.log(15);
+
+
+
+
+
+
